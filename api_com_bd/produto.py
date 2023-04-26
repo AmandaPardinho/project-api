9420f806c2a8a25799c33af72b83491fe67f35a5
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-26 08:59:05 -0300

    Nova pasta criada para melhor organização do projeto

---
+++ api_com_bd/produto.py
@@ -0,0 +1,7 @@
+class Produto:
+    def __init__(self, nome: str, descricao: str, marca: str, preco: float, id: None):
+        self.id = id
+        self.nome = nome
+        self.descricso = descricao
+        self.marca = marca
+        self.preco = preco
