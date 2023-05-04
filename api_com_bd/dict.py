203eb547b1eb3345a803f5ef267a1908f42fa98c
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-05-04 13:10:35 -0300

    Dicionário criado para facilitar a criação de produtos no postman

---
+++ api_com_bd/dict.py
@@ -0,0 +1,39 @@
+products ={
+    1: {
+        "id": 1,
+        "name": "Lapseira Técnica Pentel",
+        "description": "Lapiseira técnica com grafite 0.3mm, vermelha, pentel",
+        "weight": 50.0,
+        "brand": "Pentel",
+        "supplier": "Amazon",
+        "price": 18.99
+    },
+    2: {
+        "id": 1,
+        "name": "Lapseira Técnica Pentel",
+        "description": "Lapiseira técnica com grafite 0.3mm, vermelha, pentel",
+        "weight": 50.0,
+        "brand": "Pentel",
+        "supplier": "Amazon",
+        "price": 18.99
+    },
+    3: {
+        "id": 1,
+        "name": "Lapseira Técnica Pentel",
+        "description": "Lapiseira técnica com grafite 0.3mm, vermelha, pentel",
+        "weight": 50.0,
+        "brand": "Pentel",
+        "supplier": "Amazon",
+        "price": 18.99
+    },
+    4: {
+        "id": 1,
+        "name": "Lapseira Técnica Pentel",
+        "description": "Lapiseira técnica com grafite 0.3mm, vermelha, pentel",
+        "weight": 50.0,
+        "brand": "Pentel",
+        "supplier": "Amazon",
+        "price": 18.99
+    }
+
+}
